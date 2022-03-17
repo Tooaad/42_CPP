0ece7475423b2ddf345780db7d0ff6e18ac963a7
Gonzalo <g.pernas@alumnos.upm.es>
2022-03-17 13:13:29 +0100

    m04 ex03 cambio main

--- Mod_04/ex03/srcs/main.cpp
+++ Mod_04/ex03/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gpernas- <gpernas-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/11 18:06:53 by gpernas-          #+#    #+#             */
-/*   Updated: 2022/03/13 17:22:55 by gpernas-         ###   ########.fr       */
+/*   Updated: 2022/03/17 13:12:46 by gpernas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,71 +15,28 @@
 #include "../includes/Cure.hpp"
 #include "../includes/Character.hpp"
 
-#define COL "\033[1;36m"
-#define RES "\033[0m"
-
-int	main( void )
+int main()
 {
-	std::cout << std::endl << COL << "===== Creation test =====" << RES << std::endl;
-	IMateriaSource* src = new MateriaSource();
+	IMateriaSource *src = new MateriaSource();
 	src->learnMateria(new Ice());
 	src->learnMateria(new Cure());
-
-	ICharacter* me = new Character("me");
-
-	std::cout << std::endl << COL << "===== Equip test =====" << RES << std::endl;
-	AMateria* tmp;
+	
+	ICharacter *me = new Character("me");
+	
+	AMateria *tmp;
 	tmp = src->createMateria("ice");
 	me->equip(tmp);
-	tmp = src->createMateria("cure");
-	me->equip(tmp);
-	tmp = src->createMateria("ice");
-	me->equip(tmp);
+	
 	tmp = src->createMateria("cure");
 	me->equip(tmp);
 
-	std::cout << std::endl << COL << "===== Use test =====" << RES << std::endl;
-	ICharacter* bob = new Character("bob");
-
+	ICharacter *bob = new Character("bob");
 	me->use(0, *bob);
 	me->use(1, *bob);
-
-	std::cout << std::endl << COL << "===== Unequip test =====" << RES << std::endl;
-	for (int i = 0; i < 4; i++)
-		me->unequip(i);
-	std::cout << std::endl;
-
-	tmp = src->createMateria("ice");
-	me->equip(tmp);
-	tmp = src->createMateria("cure");
-	me->equip(tmp);
-	tmp = src->createMateria("ice");
-	me->equip(tmp);
-	tmp = src->createMateria("cure");
-	me->equip(tmp);
-	std::cout << std::endl;
-
-	std::cout << std::endl << COL << "===== Deep Copy test =====" << RES << std::endl;
-	Character*	tanos = new Character("tanos");
-	Character*	tanoscpy = new Character(*tanos);
-	Character*	tanoscpy2 = new Character("default");
-
-	*tanoscpy2 = *tanos;
-	delete tanos;
-	me->use(1, *tanoscpy);
-	me->use(0, *tanoscpy2);
-
-	std::cout << std::endl << COL << "===== Unequip and kill all =====" << RES << std::endl;
-
-	for (int i = 0; i < 4; i++)
-		me->unequip(i);
-	std::cout << std::endl;
-
-	delete tanoscpy;
-	delete tanoscpy2;
+	
 	delete bob;
 	delete me;
 	delete src;
-
+	
 	return 0;
 }
