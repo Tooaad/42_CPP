a38ef40713f0408c3958dbb05ce3d1dcc14b26bf
Tooaad <60491687+Tooaad@users.noreply.github.com>
2022-03-21 10:39:15 +0100

    Update iter.hpp

--- Mod_07/ex01/includes/iter.hpp
+++ Mod_07/ex01/includes/iter.hpp
@@ -3,13 +3,13 @@
 
 #include <iostream>
 
-template< typename T >
+template<typename T>
 void	print_val(T const & val)
 {
 	std::cout << val << std::endl;
 }
 
-template< typename T >
+template<typename T>
 void	iter(T *arry, size_t size, void (*func)(T const &))
 {
 	for (size_t i = 0; i < size; i++)
